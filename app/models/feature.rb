1c94d51cf9bdc898b66df4b2ace7eec1b41873ce
Abid Nurul Hakim <abidnurulhakim@gmail.com>
2018-05-07 20:36:19 +0700

    fix deleted_at

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,4 +1,5 @@
 class Feature < ApplicationRecord
+  acts_as_paranoid
   belongs_to :squad
   has_and_belongs_to_many :scenarios
 
--- app/models/squad.rb
+++ app/models/squad.rb
@@ -1,4 +1,5 @@
 class Squad < ApplicationRecord
+  acts_as_paranoid
   has_many :features
 
   validates :name, uniqueness: true
