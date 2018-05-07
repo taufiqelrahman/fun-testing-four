a1219340641b7942405cddbe8b1776f751feffc5
Abid Nurul Hakim <abidnurulhakim@gmail.com>
2018-05-07 21:22:38 +0700

    add includes steps in endpoint feature/:id/scenarios (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   scope '/api', module: 'api' do
     post :authenticate, to: 'authentication#authenticate', as: 'authenticate'
     get :me, to: 'users#me', as: 'users_me'
-    resources :features, except: [:create, :destroy]
+    resources :features, except: [:create] do
+      get :scenarios, on: :member, to: 'features#scenarios'
+    end
     resources :squads, except: [:destroy] do
       get :features, on: :member, to: 'squads#features'
       post :features, on: :member, to: 'squads#create_feature'
